e81eb56783d1afe60f2d71890212d09fa60a0655
Bikrom-Roy <bikromroy2001@gmail.com>
2022-10-25 00:52:30 +0600

    errorPage design and route set

--- src/Routes/Router.js
+++ src/Routes/Router.js
@@ -1,4 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
+import ErrorPage from "../components/ErrorPage/ErrorPage";
 import Home from "../components/Home/Home";
 import Main from "../layout/Main";
 
@@ -6,6 +7,7 @@ const router = createBrowserRouter([
     {
         path: '/',
         element: <Main></Main>,
+        errorElement: <ErrorPage></ErrorPage>,
         children: [
             {
                 path: '/',
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,17 +5,17 @@ const Home = () => {
     return (
         <section>
             <div className="bg-gray-100">
-                <div className="container flex flex-col items-center px-4 py-16 pb-24 mx-auto text-center lg:pb-44 md:py-32 md:px-10 lg:px-32 text-gray-900">
+                <div className="container flex flex-col items-center px-4 py-16 pb-24 mx-auto text-center lg:pb-40 md:py-32 md:px-10 lg:px-32 text-gray-900">
                     <h1 className="text-5xl font-bold leading-none sm:text-6xl xl:max-w-3xl text-gray-800">
                         Welcome To Online courses
                     </h1>
-                    <p className="mt-6 mb-8 text-lg sm:mb-12 xl:max-w-3xl dark:text-gray-900">Our excellent selection of courses has been specifically designed to help industry professionals work with today's best-of-breed automation tools. Our objective is to make our students professional and experienced in what they learn.</p>
+                    <p className="mt-6 mb-7 text-lg sm:mb-12 xl:max-w-3xl dark:text-gray-900">Our excellent selection of courses has been specifically designed to help industry professionals work with today's best-of-breed automation tools. Our objective is to make our students professional and experienced in what they learn.</p>
                     <div className="flex flex-wrap justify-center">
                         <Link to="/courses">
-                            <button className="px-8 py-3 m-2 text-lg font-semibold rounded dark:bg-violet-400 dark:text-gray-900">Get started</button>
+                            <button className="px-8 py-3 m-2 text-lg font-semibold rounded dark:bg-violet-400 dark:text-gray-900 uppercase">Get started</button>
                         </Link>
                         <Link to="/blog">
-                            <button className="px-8 py-3 m-2 text-lg border rounded text-black dark:border-gray-700">Learn more</button>
+                            <button className="px-8 py-3 m-2 text-lg border rounded text-black dark:border-gray-700 uppercase">Learn more</button>
                         </Link>
                     </div>
                 </div>
