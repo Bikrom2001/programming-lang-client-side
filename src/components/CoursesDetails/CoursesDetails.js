a13f5686f103975b82950c45064c804d8a51c346
Bikrom-Roy <bikromroy2001@gmail.com>
2022-10-27 17:01:40 +0600

    faq question add

--- src/components/CoursesDetails/CoursesDetails.js
+++ src/components/CoursesDetails/CoursesDetails.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import { Link, useLoaderData } from 'react-router-dom';
+import Pdf from "react-to-pdf";
+
+
+
+const ref = React.createRef();
 
 
 const CoursesDetails = () => {
@@ -8,24 +13,29 @@ const CoursesDetails = () => {
     return (
         <div className='bg-gray-700 pt-3 pb-5'>
             <div className="flex flex-col max-w-lg p-6 mt-5 mx-auto space-y-6 overflow-hidden rounded-lg shadow-md dark:bg-gray-900 dark:text-gray-100">
+                <div ref={ref}>
 
-                <div className="flex space-x-4">
-                    <img alt="" src={AuthorImage} className="object-cover w-12 h-12 rounded-full shadow dark:bg-gray-500" />
-                    <div className="flex flex-col space-y-1">
-                        <Link rel="noopener noreferrer" href="#" className="text-sm font-semibold">{name}</Link>
-                        <span className="text-xs dark:text-gray-400">{courseDate}</span>
+                    <div className="flex space-x-4">
+                        <img alt="" src={AuthorImage} className="object-cover w-12 h-12 rounded-full shadow dark:bg-gray-500" />
+                        <div className="flex flex-col space-y-1">
+                            <Link rel="noopener noreferrer" href="#" className="text-sm font-semibold">{name}</Link>
+                            <span className="text-xs dark:text-gray-400">{courseDate}</span>
+                        </div>
                     </div>
-                </div>
-                <div>
-                    <img src={image} alt="" className="object-cover w-full mb-4 h-60 sm:h-96 dark:bg-gray-500" />
-                    <h2 className="mb-1 text-xl font-semibold">{title}</h2>
-                    <p className="text-sm dark:text-gray-400">{description}</p>
-                </div>
+                    <div>
+                        <img src={image} alt="" className="object-cover w-full mb-4 h-60 sm:h-96 dark:bg-gray-500" />
+                        <h2 className="mb-1 text-xl font-semibold">{title}</h2>
+                        <p className="text-sm dark:text-gray-400">{description}</p>
+                    </div>
 
+                </div>
 
                 <div className="flex flex-wrap justify-between pt-3 space-x-2 text-xs dark:text-gray-400">
                     <Link to='/courses'><button type="button" className="px-8 py-3 font-semibold border rounded dark:border-gray-100 dark:text-gray-100">Back to Course</button></Link>
-                    <button type="button" className="px-8 py-3 font-semibold border rounded dark:border-gray-100 dark:text-gray-100">Download File</button>
+                    <Pdf targetRef={ref} filename="code-example.pdf">
+                        {({ toPdf }) => <button onClick={toPdf} type="button" className="px-8 py-3 font-semibold border rounded dark:border-gray-100 dark:text-gray-100">Download File</button>}
+                    </Pdf>
+
                 </div>
             </div>
 
--- src/components/FAQ/Faq.js
+++ src/components/FAQ/Faq.js
@@ -2,9 +2,30 @@ import React from 'react';
 
 const Faq = () => {
     return (
-        <div>
-            
+        <section className="dark:bg-gray-800 dark:text-gray-100">
+        <div className="container flex flex-col justify-center p-4 mx-auto md:p-8">
+            <p className="p-2 text-sm font-medium tracking-wider text-center uppercase">How it works</p>
+            <h2 className="mb-12 text-4xl font-bold leading-none text-center sm:text-5xl">GLOBAL ONLINE COURSES ABOUT</h2>
+            <div className="grid gap-10 md:gap-8 sm:p-3 md:grid-cols-2 lg:px-12 xl:px-32">
+                <div>
+                    <h3 className="font-semibold uppercase">GLOBAL ONLINE COURSES About </h3>
+                    <p className="mt-1 dark:text-gray-400">Our excellent selection of courses has been specifically designed to help industry professionals work with today's best-of-breed automation tools. Our objective is to make our students professional and experienced in what they learn.</p>
+                </div>
+                <div>
+                    <h3 className="font-semibold">Graphics Design with Motion Graphics</h3>
+                    <p className="mt-1 dark:text-gray-400">After studying Graphic Design, the opportunity of earning will be immense. You can work as a professional or freelancer and earn handsome. After successfully attending the course, you will be able to use graphic design tools & advanced techniques in a professional way.</p>
+                </div>
+                <div>
+                    <h3 className="font-semibold">Job Placements</h3>
+                    <p className="mt-1 dark:text-gray-400">Our recruitment team has significant JOB Placement RECORD for the last 14 years in finding the jobs for our students. Now in Bangladesh PeopleNTech Job Placement Team placing students in some big company in Bangladesh.</p>
+                </div>
+                <div>
+                    <h3 className="font-semibold">Our Courses - Steps to rewarding IT Career</h3>
+                    <p className="mt-1 dark:text-gray-400">Our superlative selection of courses has been specifically designed to help industry professionals work with today's best-of-breed automation tools. It is our objective to make our students professional and experienced at what they learn.</p>
+                </div>
+            </div>
         </div>
+    </section>
     );
 };
 
